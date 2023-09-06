be9074759b874e35544dd659b04eed323fa9f900
X Mister <someinkognito@gmail.com>
2023-09-06 05:18:29 +0300

    fixing filename

---
+++ src/hooks/useFetchCars.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { getCars } from '../service/cars-service';
+
+export const useFetchCars = () => {
+  const [cars, setCars] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const data = await getCars();
+        setCars(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return { cars, error, isLoading };
+};
